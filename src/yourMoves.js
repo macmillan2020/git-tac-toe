defa80ccbedad4c5b4b32b7db895a6f7fa07dd51
Priyanka Patel <ppatel@evokesolutions.com>
2019-05-07 19:17:54 -0400

    new game

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -2,9 +2,9 @@ export const x = "❌";
 export const o = "⭕️";
 
 const moves = [
-	[[o], [o], [x]],
-	[[], [x], [o]], 
-	[[x], [o], [x]]
+	[[], [], []],
+	[[], [], []], 
+	[[], [], []]
 ];
 
 export default moves;
